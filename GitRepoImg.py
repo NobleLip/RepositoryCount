fb59adf7d76bcb3a60707dfd6cff8d346e8208dd
NobleLip <skate777771@hotmail.com>
2021-10-07 14:24:30 +0100

    Update

--- GitRepoImg.py
+++ GitRepoImg.py
@@ -23,12 +23,12 @@ widths, heights = zip(*(i.size for i in images))
 total_width = sum(widths)
 max_height = max(heights)
 
-new_im = Image.new('RGB', (total_width, max_height))
+new_im = Image.new('RGBA', (total_width, max_height))
 
 x_offset = 0
 for im in images:
   new_im.paste(im, (x_offset,0))
   x_offset += im.size[0]
 
-new_im.save('Counter.png')
+new_im.save('Counter.png', "PNG")
 
